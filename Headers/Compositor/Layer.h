f29890f356e4d23fa00cb5861c852b80cd260899
Chinmay Garde <chinmaygarde@google.com>
2015-07-29 11:17:13 -0700

    Separate geometric/positioning concerns into Entity. This can be reused by Frame for initial view matrix

--- Headers/Compositor/Layer.h
+++ Headers/Compositor/Layer.h
@@ -6,98 +6,22 @@
 #define __RADARLOVE_COMPOSITOR_LAYER__
 
 #include "Core/Base.h"
-#include "Geometry/Rect.h"
-#include "Geometry/Matrix.h"
 
 #include "Compositor/Color.h"
 #include "Compositor/Frame.h"
 #include "Compositor/Primitive.h"
+#include "Compositor/Entity.h"
 
 #include <list>
 
 namespace rl {
-class Layer {
+class Layer : public Entity {
  public:
   using Ref = std::shared_ptr<Layer>;
 
 #pragma mark - Construction
 
   Layer();
-
-#pragma mark - Layer Geometry
-  /**
-   *  The frame specifies the origin and size of the layer in the coordinate
-   *  space of its parent. This is a computed property derived from the bounds
-   *  of the layer and its position.
-   *
-   *  @return the frame of the layer
-   */
-  Rect frame() const;
-
-  /**
-   *  Set the frame of the layer
-   *
-   *  @param frame the new frame
-   */
-  void setFrame(const Rect& frame);
-
-  /**
-   *  The bounds specifies the origin and size of the layer in its own
-   *  coordinate space.
-   *
-   *  @return the bounds of the layer
-   */
-  const Rect& bounds() const;
-
-  /**
-   *  Set the bounds of the layer
-   *
-   *  @param bounds the new bounds
-   */
-  void setBounds(const Rect& bounds);
-
-  /**
-   *  The position specifies the coordinates of the anchor position of the layer
-   *  relative to its parent
-   *
-   *  @return the position of the layer
-   */
-  const Point& position() const;
-
-  /**
-   *  Sets the position of the layer
-   *
-   *  @param point the new position
-   */
-  void setPosition(const Point& point);
-
-  /**
-   *  The position of the anchor point within this node in unit space
-   *
-   *  @return the anchor point
-   */
-  const Point& anchorPoint() const;
-
-  /**
-   *  Sets the new anchor point of this node
-   *
-   *  @param anchorPoint the new anchor point
-   */
-  void setAnchorPoint(const Point& anchorPoint);
-
-  /**
-   *  The transformation that is applied to the layer about its anchor point
-   *
-   *  @return the transformation applied to the node
-   */
-  const Matrix& transformation() const;
-
-  /**
-   *  Sets the transformation of the layer
-   *
-   *  @param transformation the new transformation
-   */
-  void setTransformation(const Matrix& transformation);
 
 #pragma mark - Layer Hierarchy
   /**
@@ -152,21 +76,12 @@ class Layer {
   void drawInFrame(Frame& frame);
 
  private:
-  Rect _bounds;
-  Point _position;
-  Point _anchorPoint;
-  Matrix _transformation;
-  Matrix _modelMatrix;
-  Matrix _viewMatrix;
-  bool _modelMatrixDirty;
   Color _backgroundColor;
   double _opacity;
   std::list<Ref> _sublayers;
   Layer* _superlayer;
   std::unique_ptr<Primitive> _backgroundPrimitive;
   std::unique_ptr<Primitive> _foregroundPrimitive;
-
-  const Matrix& modelMatrix();
 
   DISALLOW_COPY_AND_ASSIGN(Layer);
 };
--- Source/Compositor/Layer.cc
+++ Source/Compositor/Layer.cc
@@ -9,84 +9,13 @@
 using namespace rl;
 
 Layer::Layer()
-    : _bounds(RectZero),
-      _position(PointZero),
-      _anchorPoint(Point(0.5, 0.5)),
-      _transformation(MatrixIdentity),
-      _modelMatrix(MatrixIdentity),
-      _viewMatrix(MatrixIdentity),
-      _modelMatrixDirty(true),
+    : Entity(),
       _backgroundColor(ColorWhiteTransparent),
       _opacity(1.0),
       _sublayers(),
       _superlayer(nullptr),
       _backgroundPrimitive(),
       _foregroundPrimitive() {
-}
-
-Rect Layer::frame() const {
-  Point origin(_position.x - (_bounds.size.width * _anchorPoint.x),
-               _position.y - (_bounds.size.height * _anchorPoint.y));
-
-  return Rect(origin, _bounds.size);
-}
-
-void Layer::setFrame(const Rect& frame) {
-  setBounds(Rect(_bounds.origin, frame.size));
-  setPosition(Point(frame.origin.x + (_anchorPoint.x * frame.size.width),
-                    frame.origin.y + (_anchorPoint.y * frame.size.height)));
-}
-
-const Rect& Layer::bounds() const {
-  return _bounds;
-}
-
-void Layer::setBounds(const Rect& bounds) {
-  if (_bounds == bounds) {
-    return;
-  }
-
-  _bounds = bounds;
-  _modelMatrixDirty = true;
-}
-
-const Point& Layer::position() const {
-  return _position;
-}
-
-void Layer::setPosition(const Point& position) {
-  if (_position == position) {
-    return;
-  }
-
-  _position = position;
-  _modelMatrixDirty = true;
-}
-
-const Point& Layer::anchorPoint() const {
-  return _anchorPoint;
-}
-
-void Layer::setAnchorPoint(const Point& anchorPoint) {
-  if (_anchorPoint == anchorPoint) {
-    return;
-  }
-
-  _anchorPoint = anchorPoint;
-  _modelMatrixDirty = true;
-}
-
-const Matrix& Layer::transformation() const {
-  return _transformation;
-}
-
-void Layer::setTransformation(const Matrix& transformation) {
-  if (_transformation == transformation) {
-    return;
-  }
-
-  _transformation = transformation;
-  _modelMatrixDirty = true;
 }
 
 void Layer::addSublayer(Layer::Ref layer) {
@@ -155,24 +84,6 @@ void Layer::setOpacity(double opacity) {
   _opacity = opacity;
 }
 
-const Matrix& Layer::modelMatrix() {
-  if (!_modelMatrixDirty) {
-    return _modelMatrix;
-  }
-
-  _modelMatrixDirty = false;
-
-  const auto translated = Matrix::Translation(_position);
-
-  const auto anchored = Matrix::Translation(-_anchorPoint);
-
-  const auto sized = Matrix::Scale(_bounds.size);
-
-  _modelMatrix = translated * transformation() * sized * anchored;
-
-  return _modelMatrix;
-}
-
 void Layer::drawInFrame(Frame& frame) {
   frame.pushOpacity(_opacity);
 
@@ -195,12 +106,9 @@ void Layer::drawInFrame(Frame& frame) {
    *  Recursively visit sublayers and render as needed
    */
 
-  frame.pushViewMatrix(_viewMatrix);
-
   for (const auto& layer : _sublayers) {
     layer->drawInFrame(frame);
   }
 
-  frame.popViewMatrix();
   frame.popOpacity();
 }
