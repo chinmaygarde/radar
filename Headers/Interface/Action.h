ea814307ef47cb345b90e6a1a3c004c60ea5cda8
Chinmay Garde <chinmaygarde@gmail.com>
2015-07-31 21:36:06 -0700

    Stub out interface actions

--- Headers/Interface/Action.h
+++ Headers/Interface/Action.h
@@ -10,7 +10,73 @@
 namespace rl {
 class Action {
  public:
+  /**
+   *  Create a new action specification
+   */
+  Action();
+
+  /**
+   *  Get the total duration of a single iteration of the action. The default is
+   *  0.125
+   *
+   *  @return the action duration
+   */
+  double duration() const;
+
+  /**
+   *  Set the duration of a single iteration of the action
+   *
+   *  @param duration the action duration
+   */
+  void setDuration(double duration);
+
+  /**
+   *  Get the repeat count of the action. The default is 1
+   *
+   *  @return the action repeat count
+   */
+  uint64_t repeatCount() const;
+
+  /**
+   *  Set the repeat count of the action
+   *
+   *  @param repeatCount the new repeat count
+   */
+  void setRepeatCount(uint64_t repeatCount);
+
+  /**
+   *  If the action reverses between alternate repetitions. Repeat count must
+   *  be greater than 1 for this to take effect. The default is false
+   *
+   *  @return if the action reverses
+   */
+  bool autoReverses() const;
+
+  /**
+   *  Set if the action reverses between alternate repetitions
+   *
+   *  @param autoReverses if the action reverses
+   */
+  void setAutoReverses(bool autoReverses);
+
+  /**
+   *  Get the initial begin time offset of the action. The default is 0.
+   *
+   *  @return the begin time offset
+   */
+  double beginTime() const;
+
+  /**
+   *  Set the begin time offset of the animation
+   */
+  void setBeginTime(double beginTime);
+
  private:
+  double _duration;
+  uint64_t _repeatCount;
+  bool _autoReverses;
+  double _beginTime;
+
   DISALLOW_COPY_AND_ASSIGN(Action);
 };
 }
--- Source/Interface/Action.cc
+++ Source/Interface/Action.cc
@@ -5,3 +5,39 @@
 #include "Interface/Action.h"
 
 using namespace rl;
+
+Action::Action()
+    : _duration(0.125), _repeatCount(1), _autoReverses(false), _beginTime(0.0) {
+}
+
+double Action::duration() const {
+  return _duration;
+}
+
+void Action::setDuration(double duration) {
+  _duration = duration;
+}
+
+uint64_t Action::repeatCount() const {
+  return _repeatCount;
+}
+
+void Action::setRepeatCount(uint64_t repeatCount) {
+  _repeatCount = repeatCount;
+}
+
+bool Action::autoReverses() const {
+  return _autoReverses;
+}
+
+void Action::setAutoReverses(bool autoReverses) {
+  _autoReverses = autoReverses;
+}
+
+double Action::beginTime() const {
+  return _beginTime;
+}
+
+void Action::setBeginTime(double beginTime) {
+  _beginTime = beginTime;
+}
