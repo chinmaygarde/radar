4315b8d0df974d6473e428782486e1f6fcb31e8e
Chinmay Garde <chinmaygarde@gmail.com>
2015-07-31 22:00:15 -0700

    Minor: Add stubs for timing curve and interpolator in preparation for CAAction backing on compositor

---
+++ Headers/Compositor/Interpolator.h
@@ -0,0 +1,18 @@
+// Copyright 2015 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef __RADARLOVE_COMPOSITOR_INTERPOLATOR__
+#define __RADARLOVE_COMPOSITOR_INTERPOLATOR__
+
+#include "Core/Base.h"
+
+namespace rl {
+class Interpolator {
+ public:
+ private:
+  DISALLOW_COPY_AND_ASSIGN(Interpolator);
+};
+}
+
+#endif /* defined(__RADARLOVE_COMPOSITOR_INTERPOLATOR__) */
---
+++ Headers/Compositor/TimingCurve.h
@@ -0,0 +1,18 @@
+// Copyright 2015 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef __RADARLOVE_COMPOSITOR_TIMINGCURVE__
+#define __RADARLOVE_COMPOSITOR_TIMINGCURVE__
+
+#include "Core/Base.h"
+
+namespace rl {
+class TimingCurve {
+ private:
+ public:
+  DISALLOW_COPY_AND_ASSIGN(TimingCurve);
+};
+}
+
+#endif /* defined(__RADARLOVE_COMPOSITOR_TIMINGCURVE__) */
