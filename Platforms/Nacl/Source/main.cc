876f669d703b461aac4f068fd099125e45f4c066
Chinmay Garde <chinmaygarde@google.com>
2015-09-15 16:07:29 -0700

    Setup Native Client shell

--- Platforms/Nacl/Source/main.cc
+++ Platforms/Nacl/Source/main.cc
@@ -8,14 +8,48 @@
 
 #include <nacl_io/nacl_io.h>
 #include <Core/Core.h>
+#include <Shell/Shell.h>
+#include <Compositor/RenderSurface.h>
+#include "Sample.h"
+
+class RenderSurfaceNacl : public rl::RenderSurface {
+ public:
+  explicit RenderSurfaceNacl() : RenderSurface() {}
+
+  bool makeCurrent() { return false; }
+
+  bool present() { return false; }
+
+ private:
+  RL_DISALLOW_COPY_AND_ASSIGN(RenderSurfaceNacl);
+};
 
 class RadarLoveInstance : public pp::Instance {
  public:
-  explicit RadarLoveInstance(PP_Instance instance) : pp::Instance(instance) {}
+  explicit RadarLoveInstance(PP_Instance instance) : pp::Instance(instance) {
+    auto ppInstance = pp::Instance::pp_instance();
+    auto ppInterface = pp::Module::Get()->get_browser_interface();
+    RL_CHECK(nacl_io_init_ppapi(ppInstance, ppInterface));
 
-  virtual ~RadarLoveInstance() {}
+    _renderSurface = std::make_shared<RenderSurfaceNacl>();
+    _application = std::make_shared<sample::SampleApplication>();
+    _shell = rl::make_unique<rl::Shell>(_renderSurface, _application);
+  }
+
+  virtual ~RadarLoveInstance() {
+    _shell->shutdown();
+    _shell.reset();
+    RL_CHECK(nacl_io_uninit());
+  }
 
   virtual void HandleMessage(const pp::Var& var_message) {}
+
+ private:
+  std::unique_ptr<rl::Shell> _shell;
+  std::shared_ptr<RenderSurfaceNacl> _renderSurface;
+  std::shared_ptr<sample::SampleApplication> _application;
+
+  RL_DISALLOW_COPY_AND_ASSIGN(RadarLoveInstance);
 };
 
 class RadarLoveModule : public pp::Module {
@@ -27,6 +61,9 @@ class RadarLoveModule : public pp::Module {
   virtual pp::Instance* CreateInstance(PP_Instance instance) {
     return new RadarLoveInstance(instance);
   }
+
+ private:
+  RL_DISALLOW_COPY_AND_ASSIGN(RadarLoveModule);
 };
 
 namespace pp {
