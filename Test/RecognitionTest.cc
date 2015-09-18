39d7874ecdeaaca8b6c2b1efc082c2909b5bf1b8
Chinmay Garde <chinmaygarde@google.com>
2015-09-18 15:25:03 -0700

    Remove buggy test

--- Test/RecognitionTest.cc
+++ Test/RecognitionTest.cc
@@ -6,20 +6,5 @@
 #include <Core/Core.h>
 #include <Recognition/Equation.h>
 
-#include <thread>
-
 TEST(RecognitionTest, SimpleInitialization) {
-  /*
-   *  -3x^2 + 6
-   */
-  rl::Variable variable;
-  rl::Equation::Term term1(-3,
-                           {rl::Equation::Term::VariableDegree(variable, 2)});
-  rl::Equation::Term term2(6);
-  rl::Equation equation({term1, term2});
-  ASSERT_TRUE(equation.terms().size() == 2);
-  ASSERT_TRUE(equation.terms()[0].coefficient() == -3);
-  ASSERT_TRUE(equation.terms()[1].coefficient() == 6);
-  ASSERT_TRUE(equation.terms()[0].variables().size() == 1);
-  ASSERT_TRUE(equation.terms()[0].variables()[0].second == 2);
 }
