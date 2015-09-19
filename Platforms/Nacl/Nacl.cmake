2cc4e14dd83ff12a48a45e7d642a18c24cfecea0
Chinmay Garde <chinmaygarde@gmail.com>
2015-09-19 00:39:48 -0700

    Setup GL context and render simple scene in nacl

--- Platforms/Nacl/Nacl.cmake
+++ Platforms/Nacl/Nacl.cmake
@@ -1,22 +1,22 @@
 SET(CMAKE_SYSTEM_NAME Nacl)
 SET(CMAKE_SYSTEM_VERSION 1)
 
-include_directories(BEFORE SYSTEM /Applications/nacl_sdk/pepper_44/include)
-include_directories(BEFORE SYSTEM /Applications/nacl_sdk/pepper_44/include/newlib)
+include_directories(BEFORE SYSTEM /Applications/nacl_sdk/pepper_45/include)
+include_directories(BEFORE SYSTEM /Applications/nacl_sdk/pepper_45/include/newlib)
 
-link_directories(/Applications/nacl_sdk/pepper_44/lib/pnacl/Release)
+link_directories(/Applications/nacl_sdk/pepper_45/lib/pnacl/Release)
 
-set(CMAKE_C_COMPILER      /Applications/nacl_sdk/pepper_44/toolchain/mac_pnacl/bin/pnacl-clang)
-set(CMAKE_CXX_COMPILER    /Applications/nacl_sdk/pepper_44/toolchain/mac_pnacl/bin/pnacl-clang++)
-set(CMAKE_AR              /Applications/nacl_sdk/pepper_44/toolchain/mac_pnacl/bin/pnacl-ar CACHE STRING "")
-set(CMAKE_RANLIB          /Applications/nacl_sdk/pepper_44/toolchain/mac_pnacl/bin/pnacl-ranlib CACHE STRING "")
-set(CMAKE_FIND_ROOT_PATH  /Applications/nacl_sdk/pepper_44/toolchain/mac_pnacl)
+set(CMAKE_C_COMPILER      /Applications/nacl_sdk/pepper_45/toolchain/mac_pnacl/bin/pnacl-clang)
+set(CMAKE_CXX_COMPILER    /Applications/nacl_sdk/pepper_45/toolchain/mac_pnacl/bin/pnacl-clang++)
+set(CMAKE_AR              /Applications/nacl_sdk/pepper_45/toolchain/mac_pnacl/bin/pnacl-ar CACHE STRING "")
+set(CMAKE_RANLIB          /Applications/nacl_sdk/pepper_45/toolchain/mac_pnacl/bin/pnacl-ranlib CACHE STRING "")
+set(CMAKE_FIND_ROOT_PATH  /Applications/nacl_sdk/pepper_45/toolchain/mac_pnacl)
 
 set(CMAKE_C_FLAGS               "-U__STRICT_ANSI__" CACHE STRING "")
 set(CMAKE_CXX_FLAGS             "-U__STRICT_ANSI__" CACHE STRING "")
 
 set(RL_PLATFORM_NACL BOOL ON)
-set(RL_PNACL_FINALIZE /Applications/nacl_sdk/pepper_44/toolchain/mac_pnacl/bin/pnacl-finalize)
+set(RL_PNACL_FINALIZE /Applications/nacl_sdk/pepper_45/toolchain/mac_pnacl/bin/pnacl-finalize)
 
 set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
 set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
--- Platforms/Nacl/Source/main.cc
+++ Platforms/Nacl/Source/main.cc
@@ -5,49 +5,116 @@
 #include <ppapi/cpp/instance.h>
 #include <ppapi/cpp/module.h>
 #include <ppapi/cpp/var.h>
+#include <ppapi/cpp/graphics_3d.h>
+#include <ppapi/cpp/instance.h>
+#include <ppapi/cpp/module.h>
+#include <ppapi/cpp/var.h>
+#include <ppapi/cpp/var_array.h>
+#include <ppapi/lib/gl/gles2/gl2ext_ppapi.h>
+#include <ppapi/utility/completion_callback_factory.h>
+
+#include <GLES2/gl2.h>  // Remove
 
 #include <nacl_io/nacl_io.h>
 #include <Core/Core.h>
 #include <Shell/Shell.h>
 #include <Compositor/RenderSurface.h>
+
 #include "Sample.h"
 
 class RenderSurfaceNacl : public rl::RenderSurface {
  public:
-  explicit RenderSurfaceNacl() : RenderSurface() {}
+  explicit RenderSurfaceNacl(pp::Graphics3D& context)
+      : RenderSurface(), _context(context) {}
 
-  bool makeCurrent() { return false; }
+  bool makeCurrent() { return true; }
 
-  bool present() { return false; }
+  bool present() { return true; }
 
  private:
+  pp::Graphics3D _context;
+
   RL_DISALLOW_COPY_AND_ASSIGN(RenderSurfaceNacl);
 };
 
 class RadarLoveInstance : public pp::Instance {
  public:
-  explicit RadarLoveInstance(PP_Instance instance) : pp::Instance(instance) {
+  explicit RadarLoveInstance(PP_Instance instance)
+      : pp::Instance(instance), _callbackFactory(this) {
     auto ppInstance = pp::Instance::pp_instance();
     auto ppInterface = pp::Module::Get()->get_browser_interface();
     RL_CHECK(nacl_io_init_ppapi(ppInstance, ppInterface));
-
-    _renderSurface = std::make_shared<RenderSurfaceNacl>();
-    _application = std::make_shared<sample::SampleApplication>();
-    _shell = rl::make_unique<rl::Shell>(_renderSurface, _application);
   }
 
   virtual ~RadarLoveInstance() {
     _shell->shutdown();
     _shell.reset();
+
     RL_CHECK(nacl_io_uninit());
   }
 
   virtual void HandleMessage(const pp::Var& var_message) {}
 
+  virtual void DidChangeView(const pp::View& view) {
+    int32_t new_width = view.GetRect().width() * view.GetDeviceScale();
+    int32_t new_height = view.GetRect().height() * view.GetDeviceScale();
+
+    if (_context.is_null()) {
+      auto result = InitializeOpenGL(new_width, new_height);
+      RL_ASSERT(result);
+    } else {
+      auto result = _context.ResizeBuffers(new_width, new_height);
+      RL_ASSERT(result >= 0);
+    }
+
+    // TODO: Use new_width and new_height
+    RL_LOG("New Size %d x %d", new_width, new_height);
+    glViewport(0, 0, new_width, new_height);
+    glClearColor(0.0, 0.0, 1.0, 1.0);
+    glClear(GL_COLOR_BUFFER_BIT);
+    _context.SwapBuffers(
+        _callbackFactory.NewCallback(&RadarLoveInstance::didSwapBuffers));
+  }
+
+  void didSwapBuffers(int32_t) { RL_LOG("Finished Swapping Buffers"); }
+
+  bool InitializeOpenGL(int32_t new_width, int32_t new_height) {
+    if (!glInitializePPAPI(pp::Module::Get()->get_browser_interface())) {
+      RL_LOG("Unable to initialize GL PPAPI!");
+      return false;
+    }
+
+    const int32_t attrib_list[] = {PP_GRAPHICS3DATTRIB_ALPHA_SIZE, 8,
+                                   PP_GRAPHICS3DATTRIB_DEPTH_SIZE, 24,
+                                   PP_GRAPHICS3DATTRIB_WIDTH,      new_width,
+                                   PP_GRAPHICS3DATTRIB_HEIGHT,     new_height,
+                                   PP_GRAPHICS3DATTRIB_NONE};
+
+    _context = pp::Graphics3D(this, attrib_list);
+    if (!BindGraphics(_context)) {
+      RL_LOG("Unable to bind 3d context!");
+      _context = pp::Graphics3D();
+      glSetCurrentContextPPAPI(0);
+      return false;
+    }
+
+    glSetCurrentContextPPAPI(_context.pp_resource());
+    return true;
+  }
+
+  void InitializeRadarLove() {
+    _renderSurface = std::make_shared<RenderSurfaceNacl>(_context);
+    _application = std::make_shared<sample::SampleApplication>();
+    _shell = rl::make_unique<rl::Shell>(_renderSurface, _application);
+  }
+
  private:
   std::unique_ptr<rl::Shell> _shell;
   std::shared_ptr<RenderSurfaceNacl> _renderSurface;
   std::shared_ptr<sample::SampleApplication> _application;
+
+  pp::Graphics3D _context;
+  pp::CompletionCallbackFactory<RadarLoveInstance> _callbackFactory;
 
   RL_DISALLOW_COPY_AND_ASSIGN(RadarLoveInstance);
 };
