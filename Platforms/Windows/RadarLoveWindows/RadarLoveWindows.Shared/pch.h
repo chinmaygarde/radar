f7185e5baf9ea1f6ba0d3990ae69502a7f932ec8
Chinmay Garde <chinmaygarde@gmail.com>
2015-11-15 21:42:59 -0800

    Format imported windows files

--- Platforms/Windows/RadarLoveWindows/RadarLoveWindows.Shared/pch.h
+++ Platforms/Windows/RadarLoveWindows/RadarLoveWindows.Shared/pch.h
@@ -1,26 +1,22 @@
+
 #ifdef _WIN32
 
-	// Including SDKDDKVer.h defines the highest available Windows platform.
+/*
+ *  Including SDKDDKVer.h defines the highest available Windows platform.
+ *  If you wish to build your application for a previous Windows platform,
+ *  include WinSDKVer.h and set the _WIN32_WINNT macro to the platform you
+ *  wish to support before including SDKDDKVer.h.
+ */
 
-	// If you wish to build your application for a previous Windows platform, include WinSDKVer.h and
-	// set the _WIN32_WINNT macro to the platform you wish to support before including SDKDDKVer.h.
+#include <SDKDDKVer.h>
 
-	#include <SDKDDKVer.h>
+#ifndef WIN32_LEAN_AND_MEAN
+#define WIN32_LEAN_AND_MEAN
+#endif
 
-	#ifndef WIN32_LEAN_AND_MEAN
-	#define WIN32_LEAN_AND_MEAN
-	#endif
+#include <windows.h>
+#else
 
-	#include <windows.h>
-#elif __ANDROID__
-
-	#include <jni.h>
-	#include <errno.h>
-
-	#include <string.h>
-	#include <unistd.h>
-	#include <sys/resource.h>
-
-	#include <android/log.h>
+#error Non Windows platform file included this header
 
 #endif
