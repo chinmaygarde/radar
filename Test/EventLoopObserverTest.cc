a22c28b1707bf45b19611e05d658a8348e74b5f2
Chinmay Garde <chinmaygarde@gmail.com>
2015-12-19 22:04:37 -0800

    Add test for event loop observer priorities

--- Test/EventLoopObserverTest.cc
+++ Test/EventLoopObserverTest.cc
@@ -39,3 +39,85 @@ TEST(EventLoopObserverTest, SimpleLoopObserver) {
   ASSERT_EQ(beforeSleep, 1);
   ASSERT_EQ(afterSleep, 1);
 }
+
+TEST(EventLoopObserverTest, LoopObserverPriorities) {
+  auto orderCount = 0;
+  std::thread thread([&] {
+    auto loop = rl::core::EventLoop::Current();
+    ASSERT_NE(loop, nullptr);
+    auto obs1 = std::make_shared<rl::core::EventLoopObserver>(
+        [&](rl::core::EventLoopObserver::Activity activity) {
+          switch (activity) {
+            case rl::core::EventLoopObserver::Activity::BeforeSleep:
+              ASSERT_EQ(orderCount, 0);
+              orderCount++;
+              break;
+            case rl::core::EventLoopObserver::Activity::AfterSleep:
+              ASSERT_EQ(true, false);
+              break;
+          }
+        },
+        1 /* priority */);
+    auto obs2 = std::make_shared<rl::core::EventLoopObserver>(
+        [&](rl::core::EventLoopObserver::Activity activity) {
+          switch (activity) {
+            case rl::core::EventLoopObserver::Activity::BeforeSleep:
+              ASSERT_EQ(orderCount, 1);
+              orderCount++;
+              break;
+            case rl::core::EventLoopObserver::Activity::AfterSleep:
+              ASSERT_EQ(true, false);
+              break;
+          }
+        },
+        2 /* priority */);
+    auto obs3 = std::make_shared<rl::core::EventLoopObserver>(
+        [&](rl::core::EventLoopObserver::Activity activity) {
+          switch (activity) {
+            case rl::core::EventLoopObserver::Activity::BeforeSleep:
+              ASSERT_EQ(orderCount, 2);
+              orderCount++;
+              break;
+            case rl::core::EventLoopObserver::Activity::AfterSleep:
+              ASSERT_EQ(true, false);
+              break;
+          }
+        },
+        3 /* priority */);
+    auto obs4 = std::make_shared<rl::core::EventLoopObserver>(
+        [&](rl::core::EventLoopObserver::Activity activity) {
+          switch (activity) {
+            case rl::core::EventLoopObserver::Activity::BeforeSleep:
+              ASSERT_EQ(orderCount, 3);
+              orderCount++;
+              rl::core::EventLoop::Current()->terminate();
+              break;
+            case rl::core::EventLoopObserver::Activity::AfterSleep:
+              ASSERT_EQ(true, false);
+              break;
+          }
+        },
+        4 /* priority */);
+
+    /*
+     *  The observers are added in a random order
+     */
+    ASSERT_EQ(loop->addObserver(
+                  obs1, rl::core::EventLoopObserver::Activity::BeforeSleep),
+              true);
+    ASSERT_EQ(loop->addObserver(
+                  obs4, rl::core::EventLoopObserver::Activity::BeforeSleep),
+              true);
+    ASSERT_EQ(loop->addObserver(
+                  obs3, rl::core::EventLoopObserver::Activity::BeforeSleep),
+              true);
+    ASSERT_EQ(loop->addObserver(
+                  obs2, rl::core::EventLoopObserver::Activity::BeforeSleep),
+              true);
+
+    loop->loop();
+  });
+
+  thread.join();
+  ASSERT_EQ(orderCount, 4);
+}
