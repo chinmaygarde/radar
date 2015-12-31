f2c5f75b26d7dd840d8ed13dafe390c815eb2013
Chinmay Garde <chinmaygarde@google.com>
2015-12-30 18:58:08 -0800

    Add a stub for the socket bootstrap server

---
+++ Source/Bootstrap/SocketBootstrapServer.cc
@@ -0,0 +1,35 @@
+// Copyright 2015 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include <Core/Config.h>
+
+#if RL_CHANNELS == RL_CHANNELS_SOCKET
+
+#include <Bootstrap/BootstrapServer.h>
+
+namespace rl {
+namespace bootstrap {
+
+bool BootstrapServerAdvertise(const std::string& name,
+                              std::shared_ptr<core::Channel>
+                                  channel) {
+  RL_ASSERT_MSG(false, "WIP");
+  return false;
+}
+
+std::shared_ptr<core::Channel> BootstrapServerAcquireAdvertised(
+    const std::string& name) {
+  RL_ASSERT_MSG(false, "WIP");
+  return nullptr;
+}
+
+bool BoostrapServerStopAdvertising(std::shared_ptr<core::Channel> channel) {
+  RL_ASSERT_MSG(false, "WIP");
+  return false;
+}
+
+}  // namespace bootstrap
+}  // namespace rl
+
+#endif  // RL_CHANNELS == RL_CHANNELS_SOCKET
