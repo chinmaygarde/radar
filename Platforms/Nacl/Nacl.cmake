38f4175784ad430175b1eb907008f8121dff902e
Chinmay Garde <chinmaygarde@gmail.com>
2016-03-31 20:07:02 -0700

    Fix Nacl.cmake toolchain file for compilation on Linux

--- Platforms/Nacl/Nacl.cmake
+++ Platforms/Nacl/Nacl.cmake
@@ -33,23 +33,31 @@ include_directories(BEFORE SYSTEM ${NACL_SDK_ROOT}/include/newlib)
 
 link_directories(${NACL_SDK_ROOT}/lib/pnacl/Release)
 
+if(CMAKE_HOST_APPLE)
+  set(PNACL_TOOLCHAIN "mac_pnacl")
+elseif(CMAKE_HOST_UNIX)
+  set(PNACL_TOOLCHAIN "linux_pnacl")
+else()
+  message(FATAL_ERROR "PNACL_TOOLCHAIN variable not set for this platform")
+endif()
+
 set(CMAKE_C_COMPILER
-    ${NACL_SDK_ROOT}/toolchain/mac_pnacl/bin/pnacl-clang)
+    ${NACL_SDK_ROOT}/toolchain/${PNACL_TOOLCHAIN}/bin/pnacl-clang)
 set(CMAKE_CXX_COMPILER
-    ${NACL_SDK_ROOT}/toolchain/mac_pnacl/bin/pnacl-clang++)
+    ${NACL_SDK_ROOT}/toolchain/${PNACL_TOOLCHAIN}/bin/pnacl-clang++)
 set(CMAKE_AR
-    ${NACL_SDK_ROOT}/toolchain/mac_pnacl/bin/pnacl-ar CACHE STRING "")
+    ${NACL_SDK_ROOT}/toolchain/${PNACL_TOOLCHAIN}/bin/pnacl-ar CACHE STRING "")
 set(CMAKE_RANLIB
-    ${NACL_SDK_ROOT}/toolchain/mac_pnacl/bin/pnacl-ranlib CACHE STRING "")
+    ${NACL_SDK_ROOT}/toolchain/${PNACL_TOOLCHAIN}/bin/pnacl-ranlib CACHE STRING "")
 set(CMAKE_FIND_ROOT_PATH
-    ${NACL_SDK_ROOT}/toolchain/mac_pnacl)
+    ${NACL_SDK_ROOT}/toolchain/${PNACL_TOOLCHAIN})
 
 set(CMAKE_C_FLAGS    "-U__STRICT_ANSI__" CACHE STRING "")
 set(CMAKE_CXX_FLAGS  "-U__STRICT_ANSI__" CACHE STRING "")
 
 set(NACL BOOL ON)
 
-set(RL_PNACL_FINALIZE ${NACL_SDK_ROOT}/toolchain/mac_pnacl/bin/pnacl-finalize)
+set(RL_PNACL_FINALIZE ${NACL_SDK_ROOT}/toolchain/${PNACL_TOOLCHAIN}/bin/pnacl-finalize)
 
 set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
 set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
