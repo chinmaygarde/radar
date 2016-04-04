a2c63d540b87157eaea05226ddc4798cf9db7dcd
Chinmay Garde <chinmaygarde@gmail.com>
2016-04-03 18:03:35 -0700

    Add a benchmark runner for creating benchmark harnesses.

---
+++ Library/BenchmarkRunner/Headers/BenchmarkRunner/BenchmarkRunner.h
@@ -0,0 +1,10 @@
+// Copyright 2016 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef RADAR_BENCHMARKRUNNER_BENCHMARKRUNNER_H_
+#define RADAR_BENCHMARKRUNNER_BENCHMARKRUNNER_H_
+
+#include <benchmark/benchmark.h>
+
+#endif  // RADAR_BENCHMARKRUNNER_BENCHMARKRUNNER_H_
