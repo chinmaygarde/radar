38502ad87bb3d28350b2c38981377dae11ecfe86
Chinmay Garde <chinmaygarde@google.com>
2016-06-14 17:45:50 -0700

    Add a test harness for Library/Shell

---
+++ Library/Shell/Test/ShellTest.mm
@@ -0,0 +1,29 @@
+// Copyright 2015 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include <TestRunner/TestRunner.h>
+
+#include <Shell/Shell.h>
+
+class NullRenderSurface : public rl::coordinator::RenderSurface {
+ public:
+  NullRenderSurface(){};
+
+  bool makeCurrent() override { return false; }
+
+  bool present() override { return false; }
+
+ private:
+  RL_DISALLOW_COPY_AND_ASSIGN(NullRenderSurface);
+};
+
+RL_DECLARE_TEST_START(ShellTest)
+
+TEST(ShellTest, SimpleInitialization) {
+  auto shell = rl::shell::Shell::CreateWithCurrentThreadAsHost(
+      std::make_shared<NullRenderSurface>());
+  ASSERT_NE(shell, nullptr);
+}
+
+RL_DECLARE_TEST_END
