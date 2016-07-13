f44e3a7aaca7edd70a152c1e3b5fb211fcb9b300
Chinmay Garde <chinmaygarde@google.com>
2016-07-13 11:42:35 -0700

    PathBuilder utility for conveniently building paths.

---
+++ Library/Geometry/Headers/Geometry/PathBuilder.h
@@ -0,0 +1,45 @@
+// Copyright 2016 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef RADAR_GEOMETRY_PATHBUILDER_H_
+#define RADAR_GEOMETRY_PATHBUILDER_H_
+
+#include <Core/Macros.h>
+#include <Geometry/Path.h>
+#include <Geometry/Rect.h>
+
+namespace rl {
+namespace geom {
+
+class PathBuilder {
+ public:
+  PathBuilder();
+
+  ~PathBuilder();
+
+  Path path() const;
+
+  PathBuilder& moveTo(Point point);
+
+  PathBuilder& lineTo(Point point);
+
+  PathBuilder& quadraticCurveTo(Point point, Point controlPoint);
+
+  PathBuilder& cubicCurveTo(Point point,
+                            Point controlPoint1,
+                            Point controlPoint2);
+
+  PathBuilder& addRect(Rect rect);
+
+ private:
+  Point _current;
+  Path _prototype;
+
+  RL_DISALLOW_COPY_AND_ASSIGN(PathBuilder);
+};
+
+}  // namespace geom
+}  // namespace rl
+
+#endif  // RADAR_GEOMETRY_PATHBUILDER_H_
---
+++ Library/Geometry/Source/PathBuilder.cc
@@ -0,0 +1,60 @@
+// Copyright 2016 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include <Geometry/PathBuilder.h>
+
+namespace rl {
+namespace geom {
+
+PathBuilder::PathBuilder() = default;
+
+PathBuilder::~PathBuilder() = default;
+
+Path PathBuilder::path() const {
+  return _prototype;
+}
+
+PathBuilder& PathBuilder::moveTo(Point point) {
+  _current = point;
+  return *this;
+}
+
+PathBuilder& PathBuilder::lineTo(Point point) {
+  _prototype.addLinearComponent(_current, point);
+  _current = point;
+  return *this;
+}
+
+PathBuilder& PathBuilder::quadraticCurveTo(Point point, Point controlPoint) {
+  _prototype.addQuadraticComponent(_current, controlPoint, point);
+  _current = point;
+  return *this;
+}
+
+PathBuilder& PathBuilder::cubicCurveTo(Point point,
+                                       Point controlPoint1,
+                                       Point controlPoint2) {
+  _prototype.addCubicComponent(_current, controlPoint1, controlPoint2, point);
+  _current = point;
+  return *this;
+}
+
+PathBuilder& PathBuilder::addRect(Rect rect) {
+  _current = rect.origin;
+
+  auto topLeft = rect.origin;
+  auto bottomLeft = rect.origin + Point{0.0, rect.size.height};
+  auto bottomRight = rect.origin + Point{rect.size.width, rect.size.height};
+  auto topRight = rect.origin + Point{rect.size.width, 0.0};
+
+  _prototype.addLinearComponent(topLeft, bottomLeft)
+      .addLinearComponent(bottomLeft, bottomRight)
+      .addLinearComponent(bottomRight, topRight)
+      .addLinearComponent(topRight, topLeft);
+
+  return *this;
+}
+
+}  // namespace geom
+}  // namespace rl
