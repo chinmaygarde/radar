5b8440391425e10ba1d651f0c494272dad45e40c
Chinmay Garde <chinmaygarde@google.com>
2016-07-18 16:40:17 -0700

    Add Library/Typography stubs.

---
+++ Library/Typography/Headers/Typography/Frame.h
@@ -0,0 +1,26 @@
+// Copyright 2016 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef RADAR_TYPOGRAPHY_FRAME_H_
+#define RADAR_TYPOGRAPHY_FRAME_H_
+
+#include <Core/Macros.h>
+
+namespace rl {
+namespace typography {
+
+class Frame {
+ public:
+  Frame();
+
+  ~Frame();
+
+ private:
+  RL_DISALLOW_COPY_AND_ASSIGN(Frame);
+};
+
+}  // namespace typography
+}  // namespace rl
+
+#endif  // RADAR_TYPOGRAPHY_FRAME_H_
