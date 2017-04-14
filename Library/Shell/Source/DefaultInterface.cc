3330a78cdb19b902b7d350533c16a3a0fce17e60
Chinmay Garde <chinmaygarde@google.com>
2017-04-13 17:49:24 -0700

    [Shell] Make the default interface have a blank background.

--- Library/Shell/Source/DefaultInterface.cc
+++ Library/Shell/Source/DefaultInterface.cc
@@ -12,16 +12,7 @@ namespace rl {
 namespace shell {
 
 std::unique_ptr<interface::InterfaceDelegate> CreateDefaultInterface() {
-  /*
-   *  The default interface just creates a blank surface with a dark background.
-   */
-  auto interface = std::make_unique<interface::DefaultInterfaceDelegate>();
-  interface->setInterfaceDidFinishLaunching(
-      [](interface::Interface& interface) -> void {
-        auto& root = interface.rootEntity();
-        root.setBackgroundColor({0.2, 0.2, 0.2, 1.0});
-      });
-  return std::move(interface);
+  return std::make_unique<interface::DefaultInterfaceDelegate>();
 }
 
 }  // namespace shell
