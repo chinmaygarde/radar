2b83ba1f70baf5fa6e6070bb9b6793a52f08e3bd
Chinmay Garde <chinmaygarde@google.com>
2017-04-13 18:45:34 -0700

    [GraphicsTestRunner] Use default texture parameters.

--- Library/GraphicsTestRunner/Source/GraphicsConnectionMac.mm
+++ Library/GraphicsTestRunner/Source/GraphicsConnectionMac.mm
@@ -82,10 +82,10 @@ bool GraphicsConnectionMac::setupFramebuffer(const geom::Size& size) {
    *  Configure the texture.
    */
   glBindTexture(GL_TEXTURE_2D, _texture);
-  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
   glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, size.width, size.height, 0, GL_RGBA,
                GL_UNSIGNED_BYTE, nullptr);
 
