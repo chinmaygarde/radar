85dab6a314f1a1a91978a92789fda677308dd444
Chinmay Garde <chinmaygarde@google.com>
2017-08-10 17:06:49 -0700

    [Compositor] Fix graphics test to account for new box primitives API.

--- Library/Compositor/GraphicsTest/PrimitivesTest.cc
+++ Library/Compositor/GraphicsTest/PrimitivesTest.cc
@@ -14,7 +14,7 @@ TEST_F(GraphicsTest, SimpleBoxPrimitive) {
 
   ASSERT_TRUE(frame.begin());
 
-  rl::compositor::ColoredBoxPrimitive primitive(rl::entity::Color::Blue());
+  rl::compositor::ColoredBoxPrimitive primitive;
 
   ASSERT_TRUE(primitive.render(frame));
 
@@ -24,7 +24,7 @@ TEST_F(GraphicsTest, SimpleBoxPrimitive) {
 }
 
 TEST_F(GraphicsTestFrame, SimpleBoxPrimitiveWithFixture) {
-  rl::compositor::ColoredBoxPrimitive primitive(rl::entity::Color::Blue());
+  rl::compositor::ColoredBoxPrimitive primitive;
 
   ASSERT_TRUE(primitive.render(frame()));
 }
