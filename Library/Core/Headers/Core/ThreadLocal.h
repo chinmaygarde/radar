e7610b80861509f64903e3a2c5e821cfa10fa4a2
Chinmay Garde <chinmaygarde@google.com>
2017-08-18 17:15:15 -0700

    [Core] Revert incorrect box deletion.

--- Library/Core/Headers/Core/ThreadLocal.h
+++ Library/Core/Headers/Core/ThreadLocal.h
@@ -33,8 +33,6 @@ class ThreadLocal {
     Box(ThreadLocalDestroyCallback destroy, uintptr_t value)
         : _destroy(destroy), _value(value) {}
 
-    ~Box() { destroyValue(); }
-
     uintptr_t value() const { return _value; }
 
     void setValue(uintptr_t value) {
