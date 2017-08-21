3532ae8bfd670fd9cc7328bb7c02c02135b3b934
Chinmay Garde <chinmaygarde@google.com>
2017-08-21 14:42:38 -0700

    [Sample] Update sample to account for API updates.

--- Samples/Source/Sample.cc
+++ Samples/Source/Sample.cc
@@ -40,24 +40,18 @@ static void AddPanRecognizer(rl::interface::Interface& interface,
   });
 }
 
-static void AddGridToRoot(rl::interface::Interface& interface) {
+std::set<rl::interface::ModelEntity::Ref> AddLayers(
+    rl::interface::Interface& interface) {
   using Property = rl::layout::Variable::Property;
   auto& root = interface.rootEntity();
 
   auto rootWidth = root | Property::BoundsWidth;
   auto rootHeight = root | Property::BoundsHeight;
 
-  rl::animation::Action action;
-  action.setTimingCurveType(rl::animation::TimingCurve::Type::EaseInEaseOut);
-  action.setAutoReverses(true);
-  action.setDuration(rl::core::ClockDuration(0.5));
-  action.setRepeatCount(rl::animation::Action::RepeatCountInfinity);
-  action.setPropertyMask(rl::entity::Entity::TransformationMask);
-
-  interface.pushTransaction(std::move(action));
-
   const auto rows = 15;
   const auto cols = 15;
+
+  std::set<rl::interface::ModelEntity::Ref> layers;
 
   for (double i = 0; i < rows; i++) {
     for (double j = 0; j < cols; j++) {
@@ -65,9 +59,10 @@ static void AddGridToRoot(rl::interface::Interface& interface) {
       layer->setFrame(
           {0.0, 0.0, fmax(RND * 100.0, 25.0), fmax(RND * 100.0, 25.0)});
       layer->setBackgroundColor({RND, RND, RND, 1.0});
-
-      auto rotation = rl::geom::Matrix::RotationZ(RND * M_PI * 2.0);
-      layer->setTransformation(rotation);
+      layer->setStrokeColor({RND, RND, RND, 1.0});
+      layer->setStrokeSize(RND * 10.0);
+      layer->setTransformation(
+          rl::geom::Matrix::RotationZ(RND * M_PI * 2.0 * 3.0));
 
       auto childPositionX = *layer | Property::PositionX;
       auto childPositionY = *layer | Property::PositionY;
@@ -78,13 +73,48 @@ static void AddGridToRoot(rl::interface::Interface& interface) {
       });
 
       root.addChild(layer);
+
+      layers.emplace(std::move(layer));
     }
   }
-
-  interface.popTransaction();
+  return layers;
 }
 
-static void AddDraggableEntity(rl::interface::Interface& interface) {
+void AddGridToRoot(rl::interface::Interface& interface) {
+  auto layers = AddLayers(interface);
+
+  rl::animation::Action action;
+  action.setTimingCurveType(rl::animation::TimingCurve::Type::EaseInEaseOut);
+  action.setAutoReverses(true);
+  action.setRepeatCount(rl::animation::Action::RepeatCountInfinity);
+
+  // Update transformation.
+  action.setDuration(rl::core::ClockDuration(1.5));
+  action.setPropertyMask(rl::entity::Entity::TransformationMask);
+  auto popTransformation = interface.pushTransaction(std::move(action));
+  for (const auto& layer : layers) {
+    layer->setTransformation(
+        rl::geom::Matrix::RotationZ(RND * M_PI * 2.0 * 3.0));
+  }
+
+  // Update stroke sizes.
+  action.setDuration(rl::core::ClockDuration(0.25));
+  action.setPropertyMask(rl::entity::Entity::StrokeSizeMask);
+  auto popStrokeSizes = interface.pushTransaction(std::move(action));
+  for (const auto& layer : layers) {
+    layer->setStrokeSize(RND * 10.0);
+  }
+
+  // Update colors.
+  action.setDuration(rl::core::ClockDuration(3.0));
+  action.setPropertyMask(rl::entity::Entity::BackgroundColorMask);
+  auto popColor = interface.pushTransaction(std::move(action));
+  for (const auto& layer : layers) {
+    layer->setBackgroundColor({RND, RND, RND, 1.0});
+  }
+}
+
+void AddDraggableEntity(rl::interface::Interface& interface) {
   auto& root = interface.rootEntity();
 
   auto sub1 = interface.createEntity();
@@ -105,7 +135,7 @@ static void AddDraggableEntity(rl::interface::Interface& interface) {
   AddPanRecognizer(interface, *sub1);
 }
 
-static void AddRadialArrangement(rl::interface::Interface& interface) {
+void AddRadialArrangement(rl::interface::Interface& interface) {
   using Property = rl::layout::Variable::Property;
 
   auto& root = interface.rootEntity();
@@ -133,7 +163,7 @@ static void AddRadialArrangement(rl::interface::Interface& interface) {
   }
 }
 
-static void AddEntityWithPath(rl::interface::Interface& interface) {
+void AddEntityWithPath(rl::interface::Interface& interface) {
   auto entity = interface.createEntity();
 
   entity->setBackgroundColor(rl::entity::Color::White());
@@ -147,8 +177,8 @@ static void AddEntityWithPath(rl::interface::Interface& interface) {
   interface.rootEntity().addChild(entity);
 }
 
-static void AddSVG(rl::interface::Interface& interface,
-                   const rl::core::Bundle& bundle) {
+void AddSVG(rl::interface::Interface& interface,
+            const rl::core::Bundle& bundle) {
   rl::core::FileHandle archiveHandle(
       bundle.uriForResource(rl::core::URI{"Simple.svg"}));
 
@@ -163,6 +193,28 @@ static void AddSVG(rl::interface::Interface& interface,
   interface.rootEntity().addChild(archive->inflate(interface, map));
 }
 
+void AddStrokedPath(rl::interface::Interface& interface) {
+  rl::geom::PathBuilder builder;
+
+  builder.moveTo({200, 200});
+  builder.lineTo({400, 400});
+
+  builder.moveTo({200, 400});
+  builder.lineTo({400, 200});
+
+  builder.addRoundedRect({100, 100, 400, 400}, 50);
+
+  builder.moveTo({60, 10});
+  builder.cubicCurveTo({1200, 700}, {100, 1200}, {1600, 100});
+
+  auto entity = interface.createEntity();
+  entity->setFrame({100.00, 100.00, 100, 100});
+  entity->setPath(builder.path());
+  entity->setStrokeColor({1.0, 1.0, 1.0, 1.0});
+  entity->setStrokeSize(4);
+  interface.rootEntity().addChild(entity);
+}
+
 void SampleApplication::AddImageWithRoundedRect(
     rl::interface::Interface& interface) {
   auto entity = interface.createEntity();
@@ -171,14 +223,28 @@ void SampleApplication::AddImageWithRoundedRect(
    *  Set the rounded rect.
    */
   rl::geom::PathBuilder builder;
-  builder.addRoundedRect({{10, 10}, {300, 300}}, {50, 50, 50, 50});
+  builder.addRoundedRect({{10, 10}, {300, 400}}, {50, 50, 50, 50});
   entity->setPath(builder.path());
+  entity->setFrame({10, 500, 0.0, 0.0});
 
   /*
    *  Set the image.
    */
   auto imageURI = _bundle.uriForResource(rl::core::URI{"MonaLisa.jpg"});
   entity->setContents(rl::image::Image{std::move(imageURI)});
+  entity->setStrokeSize(10.0);
+  entity->setStrokeColor(rl::entity::Color::Teal());
+
+  interface.rootEntity().addChild(entity);
+}
+
+void AddStrokedBox(rl::interface::Interface& interface) {
+  auto entity = interface.createEntity();
+
+  entity->setBackgroundColor(rl::entity::Color::Turquoise());
+  entity->setFrame({200, 200, 400, 400});
+  entity->setStrokeColor(rl::entity::Color::White());
+  entity->setStrokeSize(2);
 
   interface.rootEntity().addChild(entity);
 }
@@ -192,7 +258,7 @@ void SampleApplication::AddEntityWithImage(
   action.setRepeatCount(rl::animation::Action::RepeatCountInfinity);
   action.setPropertyMask(rl::entity::Entity::TransformationMask);
 
-  interface.pushTransaction(std::move(action));
+  auto pop = interface.pushTransaction(std::move(action));
 
   auto entity = interface.createEntity();
 
@@ -204,8 +270,6 @@ void SampleApplication::AddEntityWithImage(
   entity->setTransformation(rl::geom::Matrix::RotationZ(M_PI));
 
   interface.rootEntity().addChild(entity);
-
-  interface.popTransaction();
 }
 
 void SampleApplication::didBecomeActive(rl::interface::Interface& interface) {
@@ -225,6 +289,8 @@ void SampleApplication::didBecomeActive(rl::interface::Interface& interface) {
   AddImageWithRoundedRect(interface);
 
   AddSVG(interface, _bundle);
+
+  AddStrokedPath(interface);
 }
 
 void SampleApplication::didEnterBackground(
