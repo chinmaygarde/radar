21442f484eed1d8a8f81c39c5a35a4d1d0c91383
Chinmay Garde <chinmaygarde@google.com>
2019-05-16 19:43:03 -0700

    [Core] Add a MachPortTest stub.

--- Library/Core/Test/MachPortTest.cc
+++ Library/Core/Test/MachPortTest.cc
@@ -5,11 +5,20 @@
 
 #include <TestRunner/TestRunner.h>
 
+#include "MachPort.h"
+
 namespace rl {
 namespace core {
 namespace testing {
 
-//
+#if RL_CHANNELS == RL_CHANNELS_MACH
+
+TEST(MachPortTest, CanCreatePorts) {
+  MachPort port(MachPort::Type::None);
+  ASSERT_FALSE(port.isValid());
+}
+
+#endif  // RL_CHANNELS == RL_CHANNELS_MACH
 
 }  // namespace testing
 }  // namespace core
